6c887f3824a10cf6bf5e1c7575e22eeeb2924fd2
brunachiarello <143562781+brunachiarello@users.noreply.github.com>
2023-11-23 12:16:11 +0000

    aaa

--- codeC.c
+++ codeC.c
@@ -2,3 +2,5 @@
 // 1. Crie uma sub-rotina que calcule o somatório de dois vetores multiplicado pelo valor máximo deles (SM -> máximo vezes somatório).
 // 2. Calcule o somatório dos vetores C e D, que foram obtidos na ESPECIFICAÇÃO GERAL, multiplicado pelo valor máximo deles.
 // 3. Salve o resultado em uma variável (SM) na memória.
+
+
