acac0e0510e0fa226d278acd8495ed073199a5bc
SophiaSilveira <sophiasilveira2003@gmail.com>
2023-11-29 12:37:03 -0300

    fim da programacao

--- tp4.asm
+++ tp4.asm
@@ -13,18 +13,21 @@ main:	la	$t0, n
 	
 	#cria c e d
 inicializa_vetores_loop:
-	beq	$t5, $t0, inicializa_max		# verifica se t5 e t0 são iguais
+	beq	$t5, $t0, inicializa_max		# verifica se t5 e t0 sï¿½o iguais
     	lw	$t6, 0($t1)		# Carrega a[i]
     	lw	$t7, 0($t2)   		# Carrega b[i]
+
     	add	$t8, $t6, $t7 		# c[i] = a[i] + b[i]
     	sub	$t9, $t6, $t7 		# d[i] = a[i] - b[i]
+
     	sw 	$t8, 0($t3)   		# Armazena c[i]
     	sw	$t9, 0($t4)   		# Armazena d[i]
+
     	addi	$t5, $t5, 1  		# Incrementa i
-    	addi	$t1, $t1, 4  		# Avança para o proximo elemento de a
-    	addi	$t2, $t2, 4  		# Avança para o proximo elemento de b
-    	addi	$t3, $t3, 4  		# Avança para o proximo elemento de c
-    	addi	$t4, $t4, 4  		# Avança para o proximo elemento de d
+    	addi	$t1, $t1, 4  		# Avanï¿½a para o proximo elemento de a
+    	addi	$t2, $t2, 4  		# Avanï¿½a para o proximo elemento de b
+    	addi	$t3, $t3, 4  		# Avanï¿½a para o proximo elemento de c
+    	addi	$t4, $t4, 4  		# Avanï¿½a para o proximo elemento de d
     	j	inicializa_vetores_loop   	
 	
 	# acha o maior valor
@@ -35,18 +38,18 @@ inicializa_max:
 	lw 	$t3, 0($t1)		# inicializa max value	com c[i]	
 
 max_value_loop:
-	beq	$t5, $t0, inicializa_soma		# verifica se t5 e t0 são iguais
+	beq	$t5, $t0, inicializa_soma		# verifica se t5 e t0 sï¿½o iguais
     	lw 	$t6, 0($t1)   		# Carrega c[i]
     	lw 	$t7, 0($t2)   		# Carrega d[i]
-    	bgt	$t6, $t3, c_maior	# verifica se c[i] é maior que max
+    	bgt	$t6, $t3, c_maior	# verifica se c[i] ï¿½ maior que max
     	
 verify_d:    	
-    	bgt	$t7, $t3, d_maior	#verifica se d[i] é maior que max
+    	bgt	$t7, $t3, d_maior	#verifica se d[i] ï¿½ maior que max
     	
 increment_max_value:
     	addi	$t5, $t5, 1  		# Incrementa i
-    	addi	$t1, $t1, 4  		# Avança para o próximo elemento de c
-    	addi	$t2, $t2, 4  		# Avança para o próximo elemento de d
+    	addi	$t1, $t1, 4  		# Avanï¿½a para o prï¿½ximo elemento de c
+    	addi	$t2, $t2, 4  		# Avanï¿½a para o prï¿½ximo elemento de d
     	j max_value_loop		
     
  c_maior:
@@ -65,26 +68,30 @@ inicializa_soma:
 	xor	$t4, $t4, $t4		# inicializa soma
 	
 soma_loop:
-	beq	$t5, $t0, inicializa_multi		# verifica se t5 e t0 são iguais
+	beq	$t5, $t0, inicializa_multi		# verifica se t5 e t0 sï¿½o iguais
     	lw 	$t6, 0($t1)   		# Carrega c[i]
     	lw 	$t7, 0($t2)   		# Carrega d[i]
+
     	add	$t4, $t4, $t6		# soma soma com a[i]
     	add	$t4, $t4, $t7		# soma soma com b[i]
+
     	addi	$t5, $t5, 1  		# Incrementa i
-    	addi	$t1, $t1, 4  		# Avança para o próximo elemento de c
-    	addi	$t2, $t2, 4  		# Avança para o próximo elemento de d
+    	addi	$t1, $t1, 4  		# Avanï¿½a para o prï¿½ximo elemento de c
+    	addi	$t2, $t2, 4  		# Avanï¿½a para o prï¿½ximo elemento de d
     	j soma_loop
     		
     	# multiplica a soma do vetor pelo maior numero encontrado	
 inicializa_multi:
 	xor	$t5, $t5, $t5		# zera i
 	xor	$t9, $t9, $t9		# inicializa res multi
+
 	lw 	$t0, sm			# t0 recebe sm
+	
 	add	$t1, $t3, $0		# t1 recebe o maior valor encontrado
-	blt	$t1, $0, max_negativo	# verifica se o meior valor encontrado é negativo
+	blt	$t1, $0, max_negativo	# verifica se o meior valor encontrado ï¿½ negativo
 
 multi_loop:
-	beq	$t5, $t1, verify_result	# verifica se i e maior valor são iguais
+	beq	$t5, $t1, verify_result	# verifica se i e maior valor sï¿½o iguais
 	add	$t9, $t9, $t4			# soma e guarda em t9
 	addi	$t5, $t5, 1			# incrementa t5
 	j multi_loop
@@ -94,14 +101,14 @@ max_negativo:
 	j	multi_loop
 	
 verify_result:
-	bgt	$t3, $0, salva_sm	# verifica se o maior valor é negativo
+	bgt	$t3, $0, salva_sm	# verifica se o maior valor ï¿½ negativo
 
 troca_sinal_result:
 	sub	$t9, $0, $t9		# troca o sinal do resultado caso o max value seja negativo
 
-	#salva na memória
+	#salva na memï¿½ria
 salva_sm:
-	sw	$t9, ($t0)		# salva o resultado em sm
+	sw	$t9, 0($t0)		# salva o resultado em sm
 	
 fim:	j	fim
 		
